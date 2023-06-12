99f328783178d4e28afde0871a8235bc0e9afd74
Anvy123 <bintangramadhan4974@gmail.com>
2023-06-12 23:24:36 +0700

    setup project & integration with Quixlab template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Pria Mahal</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('template/images/favicon.png') }}">
+    <!-- Custom Stylesheet -->
+    <link href="{{ asset('template/./plugins/tables/css/datatable/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('template/css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="loader">
+            <svg class="circular" viewBox="25 25 50 50">
+                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3"
+                    stroke-miterlimit="10" />
+            </svg>
+        </div>
+    </div>
+    <!--*******************
+        Preloader end
+    ********************-->
+
+
+    <!--**********************************
+        Main wrapper start
+    ***********************************-->
+    <div id="main-wrapper">
+
+        @include('layouts.header')
+
+        @include('layouts.sidebar')
+
+        <!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+
+            <div class="row page-titles mx-0">
+                <div class="col p-md-0">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
+                        <li class="breadcrumb-item active"><a href="javascript:void(0)">Home</a></li>
+                    </ol>
+                </div>
+            </div>
+            <!-- row -->
+
+            <div class="container-fluid mt-3">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="card gradient-1">
+                            <div class="card-body">
+                                <h3 class="card-title text-white">Products Sold</h3>
+                                <div class="d-inline-block">
+                                    <h2 class="text-white">4565</h2>
+                                    <p class="text-white mb-0">Jan - March 2019</p>
+                                </div>
+                                <span class="float-right display-5 opacity-5"><i class="fa fa-shopping-cart"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="card gradient-2">
+                            <div class="card-body">
+                                <h3 class="card-title text-white">Net Profit</h3>
+                                <div class="d-inline-block">
+                                    <h2 class="text-white">$ 8541</h2>
+                                    <p class="text-white mb-0">Jan - March 2019</p>
+                                </div>
+                                <span class="float-right display-5 opacity-5"><i class="fa fa-money"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="card gradient-3">
+                            <div class="card-body">
+                                <h3 class="card-title text-white">New Customers</h3>
+                                <div class="d-inline-block">
+                                    <h2 class="text-white">4565</h2>
+                                    <p class="text-white mb-0">Jan - March 2019</p>
+                                </div>
+                                <span class="float-right display-5 opacity-5"><i class="fa fa-users"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="card gradient-4">
+                            <div class="card-body">
+                                <h3 class="card-title text-white">Customer Satisfaction</h3>
+                                <div class="d-inline-block">
+                                    <h2 class="text-white">99%</h2>
+                                    <p class="text-white mb-0">Jan - March 2019</p>
+                                </div>
+                                <span class="float-right display-5 opacity-5"><i class="fa fa-heart"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-body pb-0 d-flex justify-content-between">
+                                        <div>
+                                            <h4 class="mb-1">Product Sales</h4>
+                                            <p>Total Earnings of the Month</p>
+                                            <h3 class="m-0">$ 12,555</h3>
+                                        </div>
+                                        <div>
+                                            <ul>
+                                                <li class="d-inline-block mr-3"><a class="text-dark"
+                                                        href="#">Day</a></li>
+                                                <li class="d-inline-block mr-3"><a class="text-dark"
+                                                        href="#">Week</a></li>
+                                                <li class="d-inline-block"><a class="text-dark" href="#">Month</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="chart-wrapper">
+                                        <canvas id="chart_widget_2"></canvas>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between">
+                                            <div class="w-100 mr-2">
+                                                <h6>Pixel 2</h6>
+                                                <div class="progress" style="height: 6px">
+                                                    <div class="progress-bar bg-danger" style="width: 40%"></div>
+                                                </div>
+                                            </div>
+                                            <div class="ml-2 w-100">
+                                                <h6>iPhone X</h6>
+                                                <div class="progress" style="height: 6px">
+                                                    <div class="progress-bar bg-primary" style="width: 80%"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Data Table</h4>
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-bordered zero-configuration">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Position</th>
+                                                <th>Office</th>
+                                                <th>Age</th>
+                                                <th>Start date</th>
+                                                <th>Salary</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Tiger Nixon</td>
+                                                <td>System Architect</td>
+                                                <td>Edinburgh</td>
+                                                <td>61</td>
+                                                <td>2011/04/25</td>
+                                                <td>$320,800</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Anjay</td>
+                                                <td>System Architect</td>
+                                                <td>Edinburgh</td>
+                                                <td>61</td>
+                                                <td>2011/04/25</td>
+                                                <td>$320,800</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #/ container -->
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
+
+        @include('layouts.footer')
+    </div>
+    <!--**********************************
+        Main wrapper end
+    ***********************************-->
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <script src="{{ asset('template/plugins/common/common.min.js') }}"></script>
+    <script src="{{ asset('template/js/custom.min.js') }}"></script>
+    <script src="{{ asset('template/js/settings.js') }}"></script>
+    <script src="{{ asset('template/js/gleek.js') }}"></script>
+    <script src="{{ asset('template/js/styleSwitcher.js') }}"></script>
+
+    <script src="{{ asset('template/./plugins/tables/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('template/./plugins/tables/js/datatable/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('template/./plugins/tables/js/datatable-init/datatable-basic.min.js') }}"></script>
+    <!-- Chartjs -->
+    <script src="{{ asset('template/./plugins/chart.js/Chart.bundle.min.js') }}"></script>
+
+    <script src="{{ asset('template/./js/dashboard/dashboard-1.js') }}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,63 @@
+<!--**********************************
+            Sidebar start
+***********************************-->
+<div class="nk-sidebar">
+    <div class="nk-nav-scroll">
+        <ul class="metismenu" id="menu">
+
+            <li class="nav-label">Dashboard</li>
+            <li>
+                <a href="widgets.html" aria-expanded="false">
+                    <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="nav-label">Master</li>
+            <li class="mega-menu mega-menu-sm">
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-grid menu-icon"></i><span class="nav-text">Master</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./layout-blank.html">Category</a></li>
+                    <li><a href="./layout-one-column.html">Product</a></li>
+                    <li><a href="./layout-two-column.html">Member</a></li>
+                    <li><a href="./layout-compact-nav.html">Supplier</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-label">Transaction</li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">Transaction</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./email-inbox.html">Expenditure</a></li>
+                    <li><a href="./email-read.html">Sale</a></li>
+                    <li><a href="./email-compose.html">Old Transaction</a></li>
+                    <li><a href="./email-compose.html">New Transaction</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-label">Report</li>
+            <li>
+                <a href="widgets.html" aria-expanded="false">
+                    <i class="icon-note menu-icon"></i><span class="nav-text">Report</span>
+                </a>
+            </li>
+
+            <li class="nav-label">Setting</li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-magnet menu-icon"></i><span class="nav-text">Setting</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./form-basic.html">User</a></li>
+                    <li><a href="./form-validation.html">Setting</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+<!--**********************************
+            Sidebar end
+***********************************-->
